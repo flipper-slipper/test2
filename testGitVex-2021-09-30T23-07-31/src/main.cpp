9d80ed70ea06cd56b96a752199a87d4823717a42
flipper-slipper <davidmbarsoum@gmail.com>
2021-09-30 19:13:01 -0400

    added cmath to main.cpp

--- testGitVex-2021-09-30T23-07-31/src/main.cpp
+++ testGitVex-2021-09-30T23-07-31/src/main.cpp
@@ -11,12 +11,14 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
+#include <cmath>
 
 using namespace vex;
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
+
   //added a comment
   
 }
