bd77e4e6f61bef9f33aadb08498e43eee03c78a6
flipper-slipper <davidmbarsoum@gmail.com>
2021-09-30 19:01:50 -0400

    added vex project

---
+++ testGitVex-2021-09-30T22-56-39/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\david                                            */
+/*    Created:      Thu Sep 30 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
